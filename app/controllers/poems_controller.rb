957dc6f27a13a7e63779e366fd9aee62db4b6531
Hirotaka Ito <h-itou@nttpc.co.jp>
2017-11-21 13:10:47 +0000

    poem20171121

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -10,6 +10,7 @@ class PoemsController < ApplicationController
   # GET /poems/1
   # GET /poems/1.json
   def show
+    @poem = Poem.find(params[:id])
   end
 
   # GET /poems/new
@@ -39,7 +40,9 @@ class PoemsController < ApplicationController
 
   # PATCH/PUT /poems/1
   # PATCH/PUT /poems/1.json
+  
   def update
+    @poem = Poem.find(params[:id])
     respond_to do |format|
       if @poem.update(poem_params)
         format.html { redirect_to @poem, notice: 'Poem was successfully updated.' }
@@ -69,6 +72,7 @@ class PoemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poem_params
-      params.require(:poem).permit(:content, :auther)
+      params.require(:poem).permit(:title, :content, :auther)
     end
 end
+
--- db/migrate/20160614051031_create_poems.rb
+++ db/migrate/20160614051031_create_poems.rb
@@ -9,3 +9,5 @@ class CreatePoems < ActiveRecord::Migration
     end
   end
 end
+
+
